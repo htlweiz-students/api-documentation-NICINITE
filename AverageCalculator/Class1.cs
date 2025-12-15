7e52d84d16ac08f9335b10c95da557dc1708aa7e
Student <foobar@sr.htlweiz.at>
2025-12-15 11:25:18 +0100

    WP_unfinished

---
+++ AverageCalculator/Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace AverageCalculator;
+
+public static class Calcutlation
+{
+    public class AverageCalculator {
+        private int length;
+        private double[] list = new dobule[length];
+
+        private int memory;
+
+        public double add(double value) {
+            if(memory > length || memory < 0) {
+                return 1; 
+            } else {
+
+            }
+        }
+
+        public double add(double[] value) {
+
+        }
+
+        public double getaverage() {
+
+        }
+
+        private double[] getElement() {
+
+        }
+
+        public int count() {
+
+        }
+
+        public AverageCalculator(double[] list, int length){
+            this.length = length;
+            this.list = list;
+        }
+
+
+    }
+}
---
+++ Program00/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace Program00 
+{
+
+}
